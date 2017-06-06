1bc93346c7438dcad81bec75f9844bd62e081760
Sam Bateman <sam@bateman.io>
2017-06-05 20:12:03 -0600

    finished, awaiting production build

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// vendor js
+// need to learn more here
+
+// vendor css
+require("purecss/build/pure-min.css");
+require("purecss/build/forms-min.css");
+
+// my js and css
+
+
+require("./styles/main.scss");
+
+
+require("./scripts/main.js");
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         {
           test: /\.scss/,
           use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.sass/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.css/,
+          use: ['style-loader', 'css-loader']
         }
       ]
    },
